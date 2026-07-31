f5773f63098148761974c441a66c446985c7071e
Maxime Bernard <maxime.bernard@infostrates.fr>
2026-07-31 15:55:56 +0200

    build: configure zsh as default shell

---
+++ files/scripts/default-shell.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+useradd --defaults --shell /usr/bin/zsh
--- files/scripts/dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-dotfiles_dir=/etc/skel/.dotfiles
-
-git clone --depth=1 https://gitlab.com/maximebern/dotfiles.git "$dotfiles_dir"
-git -C "$dotfiles_dir" remote set-url origin git@gitlab.com:maximebern/dotfiles.git
-stow --no-folding --adopt --dir="$dotfiles_dir" --target=/etc/skel .
